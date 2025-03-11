fc98eb2deb060c64e65e74a69fc0a83ec99b98ac
Dongja <129161266+jds79793@users.noreply.github.com>
2025-03-11 23:41:28 +0900

    parallel test

--- src/main/java/org/example/parallelStream.java
+++ src/main/java/org/example/parallelStream.java
@@ -1,4 +1,48 @@
 package org.example;
 
+import java.sql.Time;
+import java.util.Arrays;
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
+
 public class parallelStream {
+    List<Apple> inventory = Arrays.asList(new Apple(10), new Apple(2), new Apple(30), new Apple(200));
+
+    // 순차 처리 방식
+    List<Apple> heavyApples1 = inventory.stream()
+        .filter(apple -> apple.getWeight() > 10)
+        .collect(toList());
+
+    // 병렬 방식
+    List<Apple> heavyApples2 = inventory.parallelStream()
+        .filter((Apple a) -> a.getWeight() > 10)
+        .collect(toList());
+
+
+    public static class Apple{
+        private final int weight;
+
+        public int getWeight() {
+            return weight;
+        }
+
+        public Apple() {
+            this.weight = 0;
+        }
+
+        public Apple(int weight) {
+            this.weight = weight;
+        }
+    }
+
+    public static void main(String[] args) {
+        parallelStream ps = new parallelStream();
+        ps.heavyApples1.forEach(apple -> System.out.println(apple.getWeight()));
+        ps.heavyApples2.forEach(apple -> System.out.println(apple.getWeight()));
+    }
 }
+
+
+
+
